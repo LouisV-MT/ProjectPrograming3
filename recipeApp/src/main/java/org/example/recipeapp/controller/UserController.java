347924315de1b26c65f0ede3ad3457f5aed895c4
LouisV-MT <traceur.vu96@gmail.com>
2025-09-29 16:27:39 -0400

    Add UserController for admin user management
    Introduces UserController with endpoints for listing all users and deleting a user by ID, intended for admin functionality.

---
+++ recipeApp/src/main/java/org/example/recipeapp/controller/UserController.java
@@ -0,0 +1,30 @@
+package org.example.recipeapp.controller;
+
+import org.example.recipeapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    //For admin: list all user
+    @GetMapping("/admin/users")
+    public String listUsers(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "users";
+    }
+
+    //For admin: delete an user
+    @PostMapping("/admin/users/delete/{id}")
+    public String deleteUser(Integer id) {
+        userRepository.deleteById(id);
+        return "redirect:/admin/users";
+    }
+
+}
